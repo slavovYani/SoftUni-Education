d013a62848136ffdd7c5cec6143da4bd97813832
slavovYani <slavovyani1992@gmail.com>
2022-06-05 18:14:28 +0300

    Data Bases Lab

---
+++ DataTypesAndVariables/ConcatNames/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            string concatNames = $"{firstName}{delimiter}{lastName}";
+
+            Console.WriteLine(concatNames);
+        }
+    }
+}
