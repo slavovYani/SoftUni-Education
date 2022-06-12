4cacfbd7d6c1480719395876acadf9930dbe4b3c
slavovYani <slavovyani1992@gmail.com>
2022-06-12 22:59:56 +0300

    Arrays lab

--- FundamentalsModule/ArraysLab/08. Condense Array to Number/Program.cs
+++ FundamentalsModule/ArraysLab/08. Condense Array to Number/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08._Condense_Array_to_Number
 {
@@ -6,7 +7,26 @@ namespace _08._Condense_Array_to_Number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split();
+            int[] inputInNumber = Array.ConvertAll(input, int.Parse);
+            int[] condensed = new int[inputInNumber.Length - 1];
+
+            while (inputInNumber.Length>1)
+            {
+                for (int i = 0; i < inputInNumber.Length-1; i++)
+                {
+                 condensed[i] = inputInNumber[i] + inputInNumber[i + 1];
+
+                    if (i==inputInNumber.Length-2)
+                    {
+                    inputInNumber = condensed;
+                    condensed = new int[inputInNumber.Length - 1];
+                    }
+
+                }
+
+            }
+            Console.WriteLine(inputInNumber[0]);
         }
     }
 }
