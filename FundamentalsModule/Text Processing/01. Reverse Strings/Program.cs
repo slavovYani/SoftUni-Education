4ff56f333d84932d1e8143163bedab686c2d9102
slavovYani <slavovyani1992@gmail.com>
2022-07-31 12:59:45 +0300

    Updated Lab

--- FundamentalsModule/Text Processing/01. Reverse Strings/Program.cs
+++ FundamentalsModule/Text Processing/01. Reverse Strings/Program.cs
@@ -6,7 +6,21 @@ namespace _01._Reverse_Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string word = Console.ReadLine();
+            string reversedWord = string.Empty;
+
+            while (word!="end")
+            {
+                
+
+                for (int i = word.Length-1; i >= 0; i--)
+                {
+                    reversedWord += word[i];
+                }
+                Console.WriteLine(word + " = " + reversedWord);
+                word = Console.ReadLine();
+                reversedWord = string.Empty;    
+            }
         }
     }
 }
---
+++ FundamentalsModule/Text Processing/03.Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToRemove = Console.ReadLine();
+            string mainString = Console.ReadLine();
+
+            while (mainString.Contains(stringToRemove))
+            {
+                int startIndex = mainString.IndexOf(stringToRemove);
+                mainString = mainString.Remove(startIndex, stringToRemove.Length);
+            }
+            Console.WriteLine(mainString);
+        }
+    }
+}
